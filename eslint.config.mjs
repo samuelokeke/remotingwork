bf4ba9c5b9140e1ff956811123b15ee2e773357d
Samuel Okeke <samwizzy11@gmail.com>
2025-02-06 15:08:00 +0100

    bootstrap application

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...compat.extends("@typescript-eslint/no-require-imports", "off"),
+  // ...compat.extends("@typescript-eslint/no-require-imports", "off"),
 ];
 
 export default eslintConfig;
