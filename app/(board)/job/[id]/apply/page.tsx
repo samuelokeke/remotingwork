5bb3a542cfd493c45b7b3c48ed19995c0144c83f
Samuel Okeke <samwizzy11@gmail.com>
2025-02-09 19:41:40 +0100

    modify baseUrl for client side

--- app/(board)/job/[id]/apply/page.tsx
+++ app/(board)/job/[id]/apply/page.tsx
@@ -12,9 +12,11 @@ const ApplyFormPage = async () => {
     <div>
       <div className="h-full max-w-7xl mx-auto md:px-2 px-6 py-12">
         <div className="grid grid-cols-[4fr_1.25fr] gap-6">
-          <ApplyForm />
+          <div className="lg:col-span-1 col-span-2">
+            <ApplyForm />
+          </div>
 
-          <div>
+          <div className="lg:col-span-1 col-span-2">
             <Featured jobs={data.jobs} />
           </div>
         </div>
