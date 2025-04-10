34dc594601549df84640201b85d617c3afc2a163
Samuel Okeke <samwizzy11@gmail.com>
2025-04-10 13:24:39 +0100

    fixed page responsive

--- app/(public)/home/Hero.tsx
+++ app/(public)/home/Hero.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 const Hero = () => {
   return (
     <section className="bg-primary text-white text-center">
-      <div className="w-full h-[calc(100vh-4rem)] lg:max-w-7xl mx-auto px-6 lg:px-4 py-20">
+      <div className="w-full h-full md:h-[calc(100vh-4rem)] lg:max-w-7xl mx-auto px-6 lg:px-4 py-20">
         <div className="h-full grid place-content-center place-items-center gap-12">
           <h1 className="text-6xl font-normal leading-normal mb-12">
             Work & recruit like a professional, become a <FlipText />.
